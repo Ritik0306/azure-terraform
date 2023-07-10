4f08cd01d1272c37e5cc5624227fde9b13f5c605
Narendra Kumar <ritik3428@gmail.com>
2023-07-10 11:03:42 +0530

    feat: Added client id and client secret in provider block

--- provider.tf
+++ provider.tf
@@ -16,6 +16,8 @@ terraform {
 provider "azurerm" {
   subscription_id = "6fe625c6-7a10-4efa-bc4e-5ecc0d6e0a06"
   tenant_id = "661531e5-7111-438e-9d28-7a630db4c065"
+  client_id = "3a6682f8-d121-42ad-9cf6-bed4093a5785"
+  client_secret = "8cF8Q~y323yXVgFjRt3ZhvBz7OEHDJyjCPEvXc5H"
   features {
   }
 }
